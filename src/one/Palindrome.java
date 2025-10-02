dfa52e56270fab3adff743c100a23c6092b9fc7d
lZh1r <electroxhackerpig09@gmail.com>
2025-10-02 18:06:42 +0300

    put first lab tasks into their own package

--- src/one/Palindrome.java
+++ src/one/Palindrome.java
@@ -1,10 +1,12 @@
+package one;
+
 public class Palindrome {
     public static void main(String[] args) {
         for (int i = 0; i < args.length; i++) {
             String input = args[i];
             System.out.println("Input: " + input);
             if (isPalindrome(input)) {
-                System.out.println("Palindrome");
+                System.out.println("one.Palindrome");
             } else {
                 System.out.println("Not a palindrome");
             }
